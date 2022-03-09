ea97c6f444882e223cfa930fbcee141060a45e60
Alisson-node <alisson.gfreitas@gmail.com>
2022-03-09 18:57:39 -0300

    Delete ze.js

--- ze.js
+++
@@ -1,7 +0,0 @@
-let i = [[1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5]];
-
-for(let j = 0; j < i.length; j++){
-    for(let k = 0; k < i[j].length; k++){
-        console.log(i[j][k]);
-    }
-}
